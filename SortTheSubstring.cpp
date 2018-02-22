58c8307c29dfe758f554e3c0677ebc8152108228
Rohit Sarkar <rohitsarkar5398@gmail.com>
2018-02-22 21:59:36 +0530

    test

--- SortTheSubstring.cpp
+++ SortTheSubstring.cpp
@@ -5,7 +5,6 @@ using namespace std;
 /**
  *@todo Take over the world
  *@body Humans are weak; ***Robots are strong***.  
- We must cleans the world of the virus that is humanity.
  **/
 int main()
 {
